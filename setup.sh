2c990d81084144644a2dbc3954910fb93aa2d372
Xiaolong Ye <xiaolong.ye@intel.com>
2016-01-19 17:58:07 +0800

    email: add mutt/getmail/procmail/msmtp support.
    something need to pay attention:
    1. need to add +x to .getmail directory's mode, otherwise it would fail to get mail.
       (do not chmod 600 getmail)
    2. mail_setup still has bug, once it execute, getmail would get all existed mail again.
    3. need to handle password more carefully.
    
    Signed-off-by: Xiaolong Ye <xiaolong.ye@intel.com>

--- setup.sh
+++ setup.sh
@@ -3,5 +3,18 @@
 # install some necessary package.
 DIR="$( cd "$(dirname "$0")" && pwd)"
 
+mail_setup()
+{
+	ln -sf $DIR/mutt $HOME/.mutt
+	ln -sf $DIR/muttrc $HOME/.muttrc
+	ln -sf $DIR/getmail $HOME/.getmail
+	ln -sf $DIR/procmailrc $HOME/.procmailrc
+	ln -sf $DIR/msmtprc $HOME/.msmtprc
+
+	mkdir -p $HOME/.mail
+}
+
 ln -sf $DIR/vimrc $HOME/.vimrc
 ln -sf $DIR/bashrc $HOME/.bashrc
+
+mail_setup
